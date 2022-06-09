a8286d5a5775457aed12ef59088c876e35d99419
Rodolfo Saldanha <rodolfo.saldanha@flixbus.com>
2022-06-09 15:14:04 +0200

    Add first files

---
+++ main.py
@@ -0,0 +1,7 @@
+from skmultilearn.problem_transform import BinaryRelevance
+from skmultilearn.problem_transform import ClassifierChain
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+if __name__ == "__main__":
+    print("a")
